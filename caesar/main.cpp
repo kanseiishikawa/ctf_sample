4fa84f29180741ee9ffd77ef61ee0a5a5eedbd27
kanseiishikawa <k.ishikw@gmail.com>
2025-08-30 11:47:25 -0400

    update

--- caesar/main.cpp
+++ caesar/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-#include "shisa_decrypt.hpp"
+#include "caesar_decrypt.hpp"
 
 int main() {
     std::string file_name = "target.txt";
     std::cout<< "start\n";
-    ShisaDecrypt shisa_decrypt;
-    shisa_decrypt.decrypt(file_name);
+    CaesarDecrypt caesar_decrypt;
+    caesar_decrypt.decrypt(file_name);
     std::cout<< "finish\n";
     return 1;
 }
